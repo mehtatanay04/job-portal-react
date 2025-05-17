b160d99ff867f349a3ed2f8e55d6f941b399b974
mehtatanay04 <mehtatanay40@gmail.com>
2025-05-17 11:57:43 +0530

    Changes in firebase files

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,14 @@ import { getFirestore, serverTimestamp } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbS--zCUVTL4Od7U1p1bzX2W8vmt5aeVA",
-  authDomain: "job-portal-2dba6.firebaseapp.com",
-  projectId: "job-portal-2dba6",
-  storageBucket: "job-portal-2dba6.firebasestorage.app",
-  messagingSenderId: "632275112567",
-  appId: "1:632275112567:web:a68be6d45c79e263da6c72",
-  measurementId: "G-R74M2GV8F1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
